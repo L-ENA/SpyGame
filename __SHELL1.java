e9cc76683211313ef2befcf3482bddfb3118b0bb
Lena Schmidt <l.schmidt@sussex.ac.uk>
2018-11-20 20:21:50 +0000

    minor changes

---
+++ __SHELL1.java
@@ -0,0 +1,10 @@
+
+public class __SHELL1 extends bluej.runtime.Shell {
+public static void run() throws Throwable {
+final bluej.runtime.BJMap __bluej_runtime_scope = getScope("\\\\its-home.uscs.susx.ac.uk\\home\\ls612\\Documents\\OoP\\spy01");
+final Game game1 = (Game)__bluej_runtime_scope.get("game1");
+
+
+game1.play();
+
+}}
